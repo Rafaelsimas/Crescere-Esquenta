a304a7dc92273e13d59308dd7cadb3d1ef80162a
VictorAcarvalho <carvalho.victofa@gmail.com>
2020-11-10 15:05:43 -0300

    adiciona a funcionalidade emailSend em  script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const myElement=(id)=>{
     return document.getElementById(id);
-}
+};
 
+emailSend=()=>{
+ let sendArea = myElement('send-area');
+ sendArea.innerHTML ="<h1>As instruções para recuperar sua senha foram enviadas para o seu e-mail. </h1>";
+};
